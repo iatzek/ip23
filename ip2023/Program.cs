9bc1340fde3385b4a8d16af272be5cfe1ffafb67
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-14 17:45:21 +0100

    localize

--- ip2023/Program.cs
+++ ip2023/Program.cs
@@ -8,12 +8,14 @@ namespace ip2023
         {
             double a, b, c;
 
-            Console.WriteLine("Wprowadź współczynniki ax^2+bx+c");
-            Console.WriteLine("Wprowadź a");
+            Console.WriteLine("ax^2+bx+c");
+            Console.WriteLine("Input a");
             a = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Wprowadź b");
+
+            Console.WriteLine("Input b");
             b = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Wprowadź c");
+
+            Console.WriteLine("Input c");
             c = Double.Parse(Console.ReadLine());
 
             double delta = b * b - 4 * a * c;
@@ -23,7 +25,7 @@ namespace ip2023
                 double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
                 double x2 = (-b + Math.Sqrt(delta)) / 2 / a;
 
-                Console.WriteLine("x = {0} lub x = {1}", x1, x2);
+                Console.WriteLine("x = {0} / x = {1}", x1, x2);
             } 
             else if (delta == 0)
             {
