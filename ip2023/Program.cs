67550bc21b61aa523acf64d67d27ec1e0bc599d5
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-14 17:10:17 +0100

    Wersja PoC

--- ip2023/Program.cs
+++ ip2023/Program.cs
@@ -6,7 +6,17 @@ namespace ip2023
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, c;
+            a = 1;
+            b = 1;
+            c = -3;
+
+            double delta = b * b - 4 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("x = {0} lub x = {1}", x1, x2);
+
         }
     }
 }
