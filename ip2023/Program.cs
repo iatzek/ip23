3493719a12827d620092d55c1749df2ba1ee622a
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-14 17:24:19 +0100

    sprawdzanie zerowej delty

--- ip2023/Program.cs
+++ ip2023/Program.cs
@@ -18,13 +18,20 @@ namespace ip2023
 
             double delta = b * b - 4 * a * c;
 
-            if (delta >= 0)
+            if (delta > 0)
             {
                 double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
                 double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
 
                 Console.WriteLine("x = {0} lub x = {1}", x1, x2);
-            } else
+            } 
+            else if (delta == 0)
+            {
+                double x = (-b ) / 2 * a;
+                Console.WriteLine("x = {0}", x);
+            }
+            
+            else
             {
                 Console.WriteLine("brak rozwiązań");
             }
